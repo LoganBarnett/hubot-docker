2822ee43ed7f5f4b6272324e353816d2c7e326e3
Logan Barnett <logustus@gmail.com>
2015-10-30 02:45:20 -0700

    Need error logging already

--- scripts/docker.js
+++ scripts/docker.js
@@ -3,4 +3,11 @@ module.exports = (robot) => {
   robot.hear(/list/, (res) => {
     res.send('getting list of docker instances');
   });
+
+  robot.error((error, res) => {
+    robot.logger.error('error', error);
+    if(res) {
+      res.send('Error: ' + error);
+    }
+  });
 };
