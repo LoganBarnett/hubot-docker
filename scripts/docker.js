54c57105e55ab1282116eaeee15a4b5c550e889c
Logan Barnett <logustus@gmail.com>
2015-10-30 04:07:50 -0700

    Actually calling a docker command. Hopefully.

--- scripts/docker.js
+++ scripts/docker.js
@@ -1,7 +1,17 @@
+var exec = require('child_process').exec;
 
 module.exports = function(robot) {
   robot.hear(/list/, function(res) {
     res.send('getting list of docker instances');
+    exec('ssh 52.7.174.243 \'docker ps\'', function(error, stdout, stderr) {
+      if(error) {
+        res.send('Error: ' + error);
+      }
+      if(stderr) {
+        res.send(stderr);
+      }
+      res.send(stdout);
+    });
   });
 
   robot.error(function(error, res) {
